250b0f804a57a5fd3bdc56e834699cd7d4a968ed
yanrui <1657066071@qq.com>
2025-03-24 22:46:58 +0800

    打印cuda error

--- src/common.cu
+++ src/common.cu
@@ -293,7 +293,7 @@ testResult_t testStreamSynchronize(int ngpus, cudaStream_t* streams, ncclComm_t*
        idle = 0;
        continue;
      }
-
+     printf("cudaErr: %d\n", cudaErr);
      if (cudaErr != cudaErrorNotReady) CUDACHECK(cudaErr);
 
 #if NCCL_VERSION_CODE >= NCCL_VERSION(2,4,0)
